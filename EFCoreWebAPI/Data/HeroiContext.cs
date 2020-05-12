edf3fca763ba36eed59f62c33255042313b0264e
Evandro Padrao <evandropadrao@gmail.com>
2020-05-12 17:08:49 -0300

    Criacao dos modelos +respectivos migrations

---
+++ EFCoreWebAPI/Data/HeroiContext.cs
@@ -0,0 +1,31 @@
+﻿using EFCoreWebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreWebAPI.Data
+{
+   public class HeroiContext : DbContext
+   {
+      public DbSet<Heroi> Herois { get; set; }
+      public DbSet<Batalha> Batalhas { get; set; }
+      public DbSet<Arma> Armas { get; set; }
+      public DbSet<HeroiBatalha> HeroisBatalhas { get; set; }
+      public DbSet<IdentidadeSecreta> IdentidadeSecretas { get; set; }
+
+   protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+      {
+         optionsBuilder.UseSqlServer("Password=Epadrao00;Persist Security Info=True;User ID=sa;Initial Catalog=HeroApp;Data Source=TI000185");
+      }
+
+      protected override void OnModelCreating(ModelBuilder modelBuilder)
+      {
+         modelBuilder.Entity<HeroiBatalha>(entity =>
+         {
+            entity.HasKey(e => new { e.BatalhaId, e.HeroiId });
+         });
+      }
+   }
+}
---
+++ EFCoreWebAPI/IdentidadeSecretas.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EFCoreWebAPI
+{
+    public partial class IdentidadeSecretas
+    {
+        public int Id { get; set; }
+        public string NomeReal { get; set; }
+        public int HeroiId { get; set; }
+
+        public Herois Heroi { get; set; }
+    }
+}
